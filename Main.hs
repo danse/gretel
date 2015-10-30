b6cf5fecf90d0dac683e632887aecd962bd52fbf
danse <f.occhipinti@gmail.com>
2015-10-30 21:22:55 +0100

    clear input, prepend, initialise the page dynamically

--- Main.hs
+++ Main.hs
@@ -1,39 +1,50 @@
 import GHCJS.DOM( runWebGUI, webViewGetDomDocument )
 import GHCJS.DOM.EventM( uiWhich, EventM, newListener, addListener, target )
-import GHCJS.DOM.Document( getBody, getElementById, createElement, createTextNode, Document )
+import GHCJS.DOM.Document( getBody, getElementById, getElementsByTagName, createElement, createTextNode, Document )
 import GHCJS.DOM.Element( keyUp )
-import GHCJS.DOM.Node( appendChild )
-import GHCJS.DOM.HTMLInputElement( HTMLInputElement, getValue )
+import GHCJS.DOM.Node( appendChild, getFirstChild, insertBefore )
+import GHCJS.DOM.HTMLInputElement( HTMLInputElement, getValue, setValue )
 import GHCJS.DOM.KeyboardEvent( KeyboardEvent )
-import GHCJS.DOM.Types( toJSString, Window, IsDocument, Node )
-import Control.Monad.IO.Class
+import GHCJS.DOM.Types( toJSString, Window, IsDocument, Node, Element )
+import Control.Monad.IO.Class( MonadIO )
 
-appendLine :: (GHCJS.DOM.Types.IsDocument d, Control.Monad.IO.Class.MonadIO m) => d -> [Char] -> m (Maybe GHCJS.DOM.Types.Node)
-appendLine doc value = do
-  Just append <- getElementById doc "report"
+prependChild parent child = do
+  f <- getFirstChild parent
+  insertBefore parent child f
+
+initPage :: Document -> IO ()
+initPage doc = do
+  Just body <- getBody doc
+  Just input <- createElement doc (Just "input")
+  Just report <- createElement doc (Just "div")
+  appendChild body (Just input)
+  appendChild body (Just report)
+  listener <- newListener (onKeyUp doc report)
+  addListener input keyUp listener False
+
+appendLine :: MonadIO m => Document -> Element -> [Char] -> m (Maybe Node)
+appendLine doc report value = do
   Just newLine <- createElement doc (Just "p")
   text <- createTextNode doc (value :: [Char])
   appendChild newLine text
-  appendChild append (Just newLine)
+  prependChild report (Just newLine)
 
-onKeyUp :: Document -> EventM HTMLInputElement KeyboardEvent ()
-onKeyUp doc = do
+onKeyUp :: Document -> Element -> EventM HTMLInputElement KeyboardEvent ()
+onKeyUp doc report = do
   key <- uiWhich
   Just t <- target
   Just value <- getValue t
   if (key == 13)
     then
     do
-      appendLine doc value
+      appendLine doc report value
+      setValue t (Just "")
       return ()
     else return ()
     
 interface :: GHCJS.DOM.Types.Window -> IO ()
 interface view = do
   Just doc <- webViewGetDomDocument view
-  Just body <- getBody doc
-  Just input <- getElementById doc "input"
-  listener <- newListener (onKeyUp doc)
-  addListener input keyUp listener False
+  initPage doc
   
 main = runWebGUI interface
